23688376213115288783752d99a03765354a6580
nakamura999 <masahiro9ama9@gmail.com>
2020-08-08 18:40:09 +0900

    first commit

---
+++ src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.Customer;
+import com.example.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomerService {
+
+}

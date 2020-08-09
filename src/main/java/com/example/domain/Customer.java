2bc99308f294fb87814b5e1bbe86d35729473435
nakamura999 <masahiro9ama9@gmail.com>
2020-08-09 12:41:23 +0900

    edit

--- src/main/java/com/example/domain/Customer.java
+++ src/main/java/com/example/domain/Customer.java
@@ -14,12 +14,34 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "Costomers")
+@Table(name = "customers")
 public class Customer {
-	@Id
-	@GeneratedValue
-	private Integer id;
-	private String firstName;
-	private String lastName;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
